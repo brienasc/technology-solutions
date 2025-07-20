6176dc550f35d76f2a9868982ca6f0cb6ee9090b
Janaine Santos <janaine.santos@im.ufal.br>
2025-07-20 20:43:10 -0300

    feat: página login hmtl, css e ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,39 @@
- import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// frontend/src/app/app-routing.module.ts
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class App {
-  protected title = 'frontend-app';
-}
+ import { Routes } from '@angular/router'; 
+//import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LoginComponent } from './pages/login/login';
+//import { CadastroComponent } from './pages/cadastro/cadastro.component';
+//import { ConvidarColaboradoresComponent } from './pages/convidar-colaboradores/convidar-colaboradores.component';
+//import { MenuGerencialComponent } from './pages/menu-gerencial/menu-gerencial.component';
+
+
+// Definição das rotas (este bloco está correto e deve permanecer)
+export const routes: Routes = [
+ // { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+ //{ path: 'cadastro', component: CadastroComponent },
+  //{ path: 'convites', component: ConvidarColaboradoresComponent },
+ //{ path: 'menu-gerencial', component: MenuGerencialComponent },
+  { path: '**', redirectTo: '' } // Rota coringa para redirecionar
+];
+
+
+
+
+
+//  import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class App {
+//   protected title = 'frontend-app';
+// }
  
 
 /* import { RouterModule, Routes } from '@angular/router';
