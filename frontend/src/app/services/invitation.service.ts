5c9c84c96f12285db1b8a3cf6a8cecee0c25467c
Higor Lima <limahigor.g@gmail.com>
2025-10-02 11:57:25 -0300

    feat: adapt invitation modal to includes perfil and course id on request

--- frontend/src/app/services/invitation.service.ts
+++ frontend/src/app/services/invitation.service.ts
@@ -3,14 +3,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 // << IMPORTAÇÃO CORRETA >>
-import { Invitation } from '../interfaces/invitation.interface'; 
+import { Invitation } from '../interfaces/invitation.interface';
+
+
+type Payload = {
+  email: string;
+  perfil_id: number;
+  curso_id: string
+};
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvitationService {
-  private apiUrl = 'http://localhost:8080/api/convites'; 
-  
+  private apiUrl = 'http://localhost:8080/api/convites';
+
   constructor(private http: HttpClient) { }
 
   getInvitations(
@@ -31,10 +39,10 @@ export class InvitationService {
 
     let statusCode = undefined
 
-    if(status !== undefined){
+    if (status !== undefined) {
       statusCode = StatusEnum[status.toLowerCase() as keyof typeof StatusEnum];
     }
-    
+
     if (statusCode !== undefined) {
       params = params.set('status', statusCode);
     }
@@ -45,8 +53,7 @@ export class InvitationService {
     return this.http.get<any>(this.apiUrl, { params });
   }
 
-  createInvitation(email: string): Observable<any> {
-    const payload = { email };
+  createInvitation(payload: Payload): Observable<any> {
     return this.http.post<any>(this.apiUrl, payload);
   }
 }
